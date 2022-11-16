64c5f7327c641ca74cf8fcbe6e45bf0dbdda7ef7
Kenneth Alvarez <kvalvarez19@yahoo.com>
2022-11-15 19:47:05 -0500

    Added game initialization, display to lcd, and gaming bugs

--- wanna_dance.py
+++ wanna_dance.py
@@ -9,6 +9,16 @@ import adafruit_mpr121
 i2c = busio.I2C(board.SCL, board.SDA)
 mpr121 = adafruit_mpr121.MPR121(i2c)
 
+# THIS IS FOR THE LED SCREEN
+import adafruit_ssd1306
+from PIL import Image, ImageDraw, ImageFont
+oled = adafruit_ssd1306.SSD1306_I2C(128, 32, i2c) # Create the SSD1306 OLED class.
+oled.fill(0)
+oled.show()
+image = Image.new("1", (oled.width, oled.height)) # Create blank image for drawing.
+draw = ImageDraw.Draw(image)
+font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 16)
+
 # # LED LIGHT STRIP
 # import qwiic_led_stick
 # my_stick = qwiic_led_stick.QwiicLEDStick()
@@ -19,10 +29,27 @@ mpr121 = adafruit_mpr121.MPR121(i2c)
 # The specific capacitive sensors(aka the tiles) that will light up and need to be stepped on
 tiles = [[0, 11], [1, 10], [2, 9], [3, 8], [4, 7], [5, 6]]
 
+# Players' points
+player1 = 0
+player2 = 0
+
+# String inputted will be displayed to the LCD Screen
+def displayToLCD(strToDisplay):
+    # LED SCREEN
+    oled.fill(0) # Create blank image for drawing.
+    image = Image.new("1", (oled.width, oled.height))
+    draw = ImageDraw.Draw(image)
+    font = ImageFont.truetype("/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf", 14)
+    draw.text((0, 0), strToDisplay, font=font, fill=255) # SET THE LED SCREEN
+    oled.image(image) # Display image
+    oled.show() # show all the changes we just made
 
 # Checks if user stepped on a tile and gives them points if they did
 def checkTile(tile_to_tap):
-    time.sleep(5) # Give userse time to step on a tile 
+    global player1
+    global player2
+
+    time.sleep(5) # Give users time to step on a tile 
     if mpr121[tile_to_tap[0]].value == True and mpr121[tile_to_tap[1]].value == True:
         print("Player 1 and 2 get points")
         player1 += 10
@@ -33,17 +60,20 @@ def checkTile(tile_to_tap):
     elif mpr121[tile_to_tap[0]].value == False and mpr121[tile_to_tap[1]].value == True:
         print("Player 2 gets points but not player 1")
         player2 += 10
+    else:
+        print("No one gets any points")
 
-# Returns a random tile that both players have to step on
+# Generates two random tiles that both players have to step on
 def randomTile():
-    return tiles[random.randint(0, 5)]
+    return tiles[random.randint(0, 5)] # this is 0 to 5 because there are 6 different variations
 
 # Main game loop ran here
 def game():
-    # Players' points
-    player1 = 0
-    player2 = 0
+    global player1
+    global player2
+
     while True:
+        displayToLCD(f"Player1: {player1}\n Player2: {player2}")
         # my_stick.set_all_LED_color(214, 0, 0)
 
         tile_to_tap = randomTile()
@@ -51,13 +81,24 @@ def game():
 
         checkTile(tile_to_tap)
 
+        if player1 >= 100 and player2 >= 100: # Check if any of the players have over 100 if so end the game
+            return "It is a tie!"
+        elif player1 >= 100: 
+            return "Player 1 has won!"
+        elif player2 >= 100:
+            return "Player 2 has won!"
 
+# Wait for two users to step on a tile 3 times 
+def checkIfGameStarted():
+    steps_on_tile = 0
+
+    while steps_on_tile < 3:
+        displayToLCD("Wanna Dance?")
+        if mpr121[0].value == True and mpr121[11].value == True:
+            steps_on_tile += 1
+            print("Number of times 2 Players stepped on the same tile: " + str(steps_on_tile))
+            time.sleep(2)
         
-
-        # for i in range(12):
-        #     if mpr121[i].value:
-        #         print(f"Twizzler {i} touched!")
-        # time.sleep(0.25)  # Small delay to keep from spamming output messages.
 
 # Game initialization and ending done here
 def main():
@@ -67,8 +108,14 @@ def main():
     #     return
     # print("\nLED Stick ready!")
 
-    # Game
-    game()
+    # Wait for users to step on squares
+    checkIfGameStarted()
+
+    # Start the game
+    winner = game()
+
+    # Show game results on LCD
+    displayToLCD(winner)
 
 if __name__ == "__main__":
     main()
